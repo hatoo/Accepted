a8f6e6f68488c82fe6aa1af62acff73e7506ab39
hatoo <hato2000@gmail.com>
2018-08-23 23:24:37 +0900

    sandbox

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+extern crate termion;
+
+use std::io::{stdin, stdout, Write};
+use termion::event::{Event, Key, MouseEvent};
+use termion::input::{MouseTerminal, TermRead};
+use termion::raw::IntoRawMode;
+
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
+
+    let (tx, rx) = channel();
+
+    thread::spawn(move || {
+        for c in stdin.events() {
+            if let Ok(evt) = c {
+                tx.send(evt).unwrap();
+            }
+        }
+    });
+
+    loop {
+        while let Ok(evt) = rx.recv_timeout(Duration::from_millis(16)) {
+            match evt {
+                Event::Key(Key::Char('q')) => {
+                    return;
+                }
+                evt => {
+                    write!(stdout, "{:?}\r\n", evt);
+                }
+            }
+        }
+    }
 }
