1839b8d3cd4897b42591acb39fa596137a39e632
hatoo <hato2000@gmail.com>
2018-08-25 14:38:03 +0900

    Scrolling

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,22 @@ fn refresh_screen<T: Write>(w: &mut T) {
     write!(w, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
 }
 
+fn get_rows(s: &[char], width: usize) -> usize {
+    let mut x = 0;
+    let mut y = 1;
+
+    for &c in s {
+        let w = c.width().unwrap_or(0);
+        if x + w < width {
+            x += w;
+        } else {
+            y += 1;
+            x = w;
+        }
+    }
+    y
+}
+
 impl Window {
     pub fn new() -> Self {
         Window {
@@ -32,14 +48,36 @@ impl Window {
         }
     }
 
+    fn windows_size() -> (usize, usize) {
+        let (cols, rows) = termion::terminal_size().unwrap();
+        (rows as usize, cols as usize)
+    }
+
+    fn set_offset(&mut self) {
+        if self.row_offset >= self.cursor.row {
+            self.row_offset = self.cursor.row;
+        } else {
+            let (rows, cols) = Self::windows_size();
+            let mut i = self.cursor.row + 1;
+            let mut sum = 0;
+            while i > 0 && sum + get_rows(&self.buffer[i - 1], cols) <= rows {
+                sum += get_rows(&self.buffer[i - 1], cols);
+                i -= 1;
+            }
+            self.row_offset = max(i, self.row_offset);
+        }
+    }
+
     pub fn cursor_left(&mut self) {
         if self.cursor.col != 0 {
             self.cursor.col -= 1;
         }
+        self.set_offset();
     }
 
     pub fn cursor_right(&mut self) {
         self.cursor.col = min(self.buffer[self.cursor.row].len(), self.cursor.col + 1);
+        self.set_offset();
     }
 
     pub fn cursor_up(&mut self) {
@@ -47,11 +85,13 @@ impl Window {
             self.cursor.row -= 1;
             self.cursor.col = min(self.buffer[self.cursor.row].len(), self.cursor.col);
         }
+        self.set_offset();
     }
 
     pub fn cursor_down(&mut self) {
         self.cursor.row = min(self.buffer.len() - 1, self.cursor.row + 1);
         self.cursor.col = min(self.buffer[self.cursor.row].len(), self.cursor.col);
+        self.set_offset();
     }
 
     pub fn insert(&mut self, c: char) {
@@ -67,6 +107,7 @@ impl Window {
             self.buffer[self.cursor.row].insert(self.cursor.col, c);
             self.cursor.col += 1;
         }
+        self.set_offset();
     }
 
     pub fn backspase(&mut self) {
@@ -79,6 +120,7 @@ impl Window {
             self.buffer[self.cursor.row - 1].append(&mut line);
             self.cursor.row -= 1;
         }
+        self.set_offset();
     }
 
     pub fn draw<T: Write>(&self, out: &mut T) {
