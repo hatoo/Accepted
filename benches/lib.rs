84f674ef4914298a279c3bcc3af4107fd565774f
hatoo <hato2000@gmail.com>
2019-01-14 16:15:21 +0900

    Add benches

---
+++ benches/lib.rs
@@ -0,0 +1,19 @@
+#![feature(test)]
+
+extern crate test;
+
+use accepted::{Buffer, BufferMode};
+use termion::event::{Event, Key};
+use test::Bencher;
+
+#[bench]
+fn bench_add_1(b: &mut Bencher) {
+    let syntax_parent = accepted::syntax::SyntaxParent::default();
+    let buf = Buffer::new(&syntax_parent);
+    let mut state = BufferMode::new(buf);
+
+    state.event(Event::Key(Key::Char('i')));
+    b.iter(move || {
+        state.event(Event::Key(Key::Char('a')));
+    });
+}
