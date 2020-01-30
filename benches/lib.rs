974f23881883f36b27f68c220c70536b112da15d
hatoo <hato2000@gmail.com>
2020-01-30 18:23:14 +0900

    Make bench work

--- benches/lib.rs
+++ benches/lib.rs
@@ -6,14 +6,14 @@ use test::Bencher;
 
 use termion::event::{Event, Key};
 
-use accepted::{config, Buffer, BufferMode};
+use accepted::{config, core::buffer::RopeyCoreBuffer, Buffer, BufferMode};
 
 #[bench]
 fn bench_insert_1(b: &mut Bencher) {
     let syntax_parent = accepted::syntax::SyntaxParent::default();
     let config = config::ConfigWithDefault::default();
     let buf = Buffer::new(&syntax_parent, &config);
-    let mut state = BufferMode::new(buf);
+    let mut state = BufferMode::<RopeyCoreBuffer>::new(buf);
 
     state.event(Event::Key(Key::Char('i')));
     b.iter(move || {
